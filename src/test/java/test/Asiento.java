f6aaf45cfd44f0fe194378857482b2ec3ed51073
sahelyromero <sahromeroca@unal.edu.co>
2023-08-18 19:53:41 -0500

    Creación clase Asiento
    Se sube la clase asiento con sus respectivos atributos y metodos

---
+++ src/test/java/test/Asiento.java
@@ -0,0 +1,21 @@
+package test;
+
+public class Asiento {
+	
+	String color;
+	int precio;
+	int registro;
+	
+	void cambiarColor(String color) {
+		if (color == "rojo")
+			this.color = color;
+		else if (color == "verde")
+			this.color = color;
+		else if (color == "amarillo")
+			this.color = color;
+		else if (color == "blanco")
+			this.color = color;
+		else if (color == "negro")
+			this.color = color;
+	}
+}
