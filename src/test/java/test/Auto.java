8ec77197893b1ee890a55f377d8ca86334d29dd2
sahelyromero <sahromeroca@unal.edu.co>
2023-08-18 20:14:36 -0500

    Se sube la clase Auto
    No he terminado todos los metodos, sospecho que uno está malo.

---
+++ src/test/java/test/Auto.java
@@ -0,0 +1,16 @@
+package test;
+
+public class Auto {
+	
+	String modelo;
+	int precio;
+	Asiento[] asientos = new Asiento[10];
+	String marca;
+	Motor motor;
+	int registro;
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+		return(cantidadCreados);
+	}
+}
